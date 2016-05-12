ee4015f2efd329c6fb4d5711243dbecbde60031d
rojanomartinez <jesus.3fdsaupoo@gmail.com>
2016-05-12 13:39:23 -0500

    Update acciones.js

--- js/acciones.js
+++ js/acciones.js
@@ -2,7 +2,7 @@
 $(document).ready(function(e) {
     document.addEventListener( 'deviceready', function() {
 		$('#btnbeep').on('tap',function() {
-			navigator.notification.beep(1);
+			navigator.notification.beep(2);
 		});
 		$('#btnvibrar').on('tap',function() {
 			navigator.notification.vibrate(2000);
